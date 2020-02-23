77b8ff5005061597899c6b68257f5cd50e0dd4e2
Riki Afifuddin <rikinur34@gmail.com>
2020-02-23 19:38:42 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
         <img src="https://instagram.fcgk4-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83986915_846199862473686_8488581279439126528_n.jpg?_nc_ht=instagram.fcgk4-1.fna.fbcdn.net&_nc_ohc=SuER8QQeJIEAX_qC3Yh&oh=c403de72879747a51826568abb6eca7e&oe=5E8C23A3" class="rounded-circle">
       </div>
       <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href='#'>Add new post</a>
+            </div>
             <div class="d-flex">
                 <div class=pr-5><strong>24</strong> post</div>
                 <div class=pr-5><strong>1399</strong> follower</div>
