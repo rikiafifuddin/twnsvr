5a6315ac2f941abb4b3e41eac771680e8cad572e
Riki Afifuddin <rikinur34@gmail.com>
2020-03-02 14:16:08 +0700

    update

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,29 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
- public function __construct(){
-     $this->middleware('auth'); //must be login
- }
- public function create(){
-     return view('posts.create');
- }
- 
- public function store(){
-     $data = request()->validate([
-         'caption' => 'required',
-         'image' => ['required','image'],
-     ]);
-    
-    $imagePath= request('image')->store('uploads', 'public');
-    auth()->user()->posts()->create([
-        'caption' =>$data['caption'],
-        'image' => $imagePath,
-    ]);
+    public function __construct()
+    {
+        $this->middleware('auth'); //must be login
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
 
-    return redirect('/profile/' . auth()->user()->id);  
-}
+    public function store()
+    {
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+
+        $imagePath = request('image')->store('uploads', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
+    }
 }
